aa7294fc69ea16d3412ac0fd8bdb8542ecacc7c7
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-15 22:45:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,19 +1,19 @@
 chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+  return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-  return array
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element) {
-  return [...array, element]
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
-  return array
+  array.push(element);
+  return array;
 }
