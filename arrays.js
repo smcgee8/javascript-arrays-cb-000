09108b081af094a64443aed9e948a6c9621b28ae
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-15 23:00:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,5 +37,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1)
+  array = array.slice(0, array.length - 1)
+  return array
 }
